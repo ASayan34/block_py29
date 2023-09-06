b109742a6788dead8593a2c8a63155f79bfd7286
ASayan34 <abibillaevvsayan@gmail.com>
2023-09-06 19:16:00 +0600

    logic with activation code

--- applications/account/views.py
+++ applications/account/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from applications.account.serializers import RegisterSerializer
+
+User = get_user_model()
 
 
 class RegisterAPIView(APIView):
@@ -12,3 +15,16 @@ class RegisterAPIView(APIView):
         serializer.save()
 
         return Response('Вы успешно зарегистровались. Вам отправлено письмо на почту с активацией', status=201)
+
+
+class ActivationAPIView(APIView):
+    def get(self, request, activation_code):
+        # try:
+        #     user = User.objects.get(activation_code=activation_code)
+        # except User.DoesNotExist:
+        #     return Response('Неправильной код активации', status=400)
+        user = get_object_or_404(User, activation_code=activation_code)
+        user.is_active = True
+        user.activation_code = ''
+        user.save(update_fields=['is_active', 'activation_code'])
+        return Response('Успешно', status=200)
